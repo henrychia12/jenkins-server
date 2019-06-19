440b543d8ceed09b2531b8ead46dfb2b738318fe
henrychia12 <henry_chia21@hotmail.co.uk>
2019-06-19 20:41:35 +0100

    minor change to install.sh & finished update.sh

--- update.sh
+++ update.sh
@@ -5,3 +5,6 @@ sudo systemctl stop jenkins
 
 # using user - jenkins to install latest jenkins WAR file 
 sudo su - jenkins -c "wget https://updates.jenkins-ci.org/latest/jenkins.war"
+
+# start jenkins servie again
+sudo systemctl start jenkins
