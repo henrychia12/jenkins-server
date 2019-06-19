2834f4c5944905a5af2bc29ece64ef626b700b2c
henrychia12 <henry_chia21@hotmail.co.uk>
2019-06-19 22:07:00 +0100

    altered update.sh

--- update.sh
+++ update.sh
@@ -4,7 +4,7 @@
 sudo systemctl stop jenkins
 
 # delete outdated jenkins WAR file
-sudo su - jenkins -c "sudo rm -r /home/jenkins/jenkins.war"
+sudo rm -r /home/jenkins/jenkins.war
 
 # using user - jenkins to install latest jenkins WAR file 
 sudo su - jenkins -c "wget https://updates.jenkins-ci.org/latest/jenkins.war"
