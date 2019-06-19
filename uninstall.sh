2e991b7a7585c9143400d509371e05468b075c28
henrychia12 <henry_chia21@hotmail.co.uk>
2019-06-19 22:37:49 +0100

    slight correction to uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -4,7 +4,7 @@
 sudo systemcl stop jenkins
 
 # remove jenkins service
-sudo rm -r /etc/systemctl/system/jenkins.service
+sudo rm -r /etc/systemd/system/jenkins.service
 
 # deleting jenkins user using root
 sudo su - root -c "userdel -r jenkins"
