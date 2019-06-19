3022d9acf6b220e6d5869f57cce425960b1ec41f
henrychia12 <henry_chia21@hotmail.co.uk>
2019-06-19 20:32:42 +0100

    jenkins-service corrected to jenkins.service

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ sudo usermod --shell /bin/bash jenkins
 sudo su - jenkins -c "wget http://updates.jenkins-ci.org/latest/jenkins.war"
 
 # copy jenkins-service file to systemd
-cp ./jenkins-service /etc/systemd/system
+sudo cp ./jenkins.service /etc/systemd/system
 
 # new service added, systemd needs to be reloaded
 sudo systemctl daemon-reload
---
+++ update.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# stop jenkins service
+sudo systemctl stop jenkins
+
+# using user - jenkins to install latest jenkins WAR file 
+sudo su - jenkins -c "wget https://updates.jenkins-ci.org/latest/jenkins.war"
