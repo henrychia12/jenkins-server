38c40a621b2fadd35e2b0fe8f3b6be55514e998e
Ubuntu <henry@JenkinsHostVM.k24sikqgk3uuxbdxxqbj0kr3qa.zx.internal.cloudapp.net>
2019-06-26 10:47:09 +0000

    added script directory for script files

--- scripts/install.sh
+++ scripts/install.sh
@@ -9,11 +9,14 @@ sudo useradd --create-home jenkins
 # set jenkins default shell to bash
 sudo usermod --shell /bin/bash jenkins
 
+# deletes jenkins WAR file, if any
+sudo rm -r /home/jenkins/jenkins.war
+
 # briefly carries a task on user - jenkins to obtain the jenkins WAR file.
 sudo su - jenkins -c "wget http://updates.jenkins-ci.org/latest/jenkins.war"
 
 # copy jenkins-service file to systemd
-sudo cp ./jenkins.service /etc/systemd/system
+sudo cp ../jenkins.service /etc/systemd/system
 
 # new service added, systemd needs to be reloaded
 sudo systemctl daemon-reload
